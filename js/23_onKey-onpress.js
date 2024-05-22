772d827a0b2b56e7ac55897f6efb72f9ba0de4b2
GeonHwiAhn <169108642+GeonHwiAhn@users.noreply.github.com>
2024-05-22 17:49:21 +0900

    유효성검사

--- js/23_onKey-onpress.js
+++ js/23_onKey-onpress.js
@@ -2,6 +2,9 @@ function 기능만들기() {
     const 기능 = document.getElementById("영문이름");
     기능.value = 기능.value.toUpperCase();
   }
-  function 엔터기능() {
+  function 엔터기능(e) {
+    //만약에 엔터 키를 눌렀을 때
+    if(e.key === 'Enter'){
     alert("입력한 내용이 전달되었습니다.");
+    }
   }
